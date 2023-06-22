5ddafc5c3897d5e7b8de3a736d99c44c6315a89c
Luis Fernando Pardo <lfern70@gmail.com>
2023-06-22 18:28:25 +0200

    dashboard page

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,14 +1,45 @@
 const models = require('../models');
+let createError = require('http-errors');
 
 exports.get_landing = function(req, res, next) {
-    models.Exchange.findAll().then(exchanges => {
+    Promise.all([
+        models.StrategyInstance.findAll({
+            where: {running: true},
+            include: [
+                {
+                    association: models.StrategyInstance.Strategy,
+                    include: [
+                        models.Strategy.Account
+                    ]
+                }
+            ],
+        }),
+        models.StrategyInstanceEvent.findAll({
+            order:[
+                ['createdAt', 'DESC']
+            ],
+            include: [
+                {
+                    association: models.StrategyInstanceEvent.StrategyInstance,
+                    include: [
+                        models.StrategyInstance.Strategy
+                    ]
+                }
+            ],
+            limit: 100
+        }),
+        models.Account.findAll()
+    ]).then(result => {
         res.render('landing', {
-            title: 'Express',
+            title: 'Dashboard',
             user: req.user,
-            exchanges: exchanges
+            instances: result[0],
+            events: result[1],
+            accounts: result[2],
         });
     }).catch(ex => {
         return next(createError(500, ex));
     });
+
 }
 
