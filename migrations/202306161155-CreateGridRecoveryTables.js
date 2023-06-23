bb694059ba0da6533cc59cfd8a57f9fbf4efbec6
Luis Fernando Pardo <lfern70@gmail.com>
2023-06-23 07:58:17 +0200

    add recovery tables

---
+++ migrations/202306161155-CreateGridRecoveryTables.js
@@ -0,0 +1,125 @@
+'use strict'
+
+/** @typedef {import('sequelize').QueryInterface} QueryInterface */
+/** @typedef {import('sequelize')} sequelize */
+
+module.exports = {
+    /**
+     * 
+     * @param {QueryInterface} queryInterface 
+     * @param {sequelize} Sequelize 
+     * @returns 
+     */
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.sequelize.transaction(async transaction => {
+            
+            await queryInterface.createTable('strategy_instance_recovery_grids', {
+                id: {
+                    allowNull: false,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER,
+                    autoIncrement: true
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                strategy_instance_grid_id: {
+                    allowNull: false,
+                    type: Sequelize.INTEGER,
+                    references: {
+                        model: {
+                            tableName: 'strategy_instance_grid'
+                        },
+                        key: 'id'
+                    }
+                },
+                order_qty: {
+                    allowNull: true,
+                    type: Sequelize.DECIMAL(30,15),
+                },
+                side: {
+                    allowNull: true,
+                    type: Sequelize.ENUM('buy', 'sell'),
+                },
+                exchange_order_id: {
+                    allowNull: true,
+                    type: Sequelize.STRING,
+                }
+            },
+            {
+                transaction
+            });
+
+            // Table broadcast transactions
+            await queryInterface.createTable('strategy_instance_recovery_grid_orders', {
+                id: {
+                    allowNull: false,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER,
+                    autoIncrement: true
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                strategy_instance_id: {
+                    allowNull: false,
+                    type: Sequelize.INTEGER,
+                    references: {
+                        model: {
+                            tableName: 'strategy_instances'
+                        },
+                        key: 'id'
+                    }
+                },
+                status: {
+                    allowNull: false,
+                    type: Sequelize.ENUM('downloaded', 'executed', 'executed-order-created', 'all-downloaded')
+                },
+                order_id: {
+                    allowNull: true,
+                    type: Sequelize.STRING,
+                    unique: true
+                },
+                order: {
+                    allowNull:true,
+                    type: Sequelize.JSONB,
+                },
+                execution_timestamp: {
+                    allowNull: true,
+                    type: Sequelize.INTEGER
+                },
+                creation_timestamp: {
+                    allowNull: true,
+                    type: Sequelize.INTEGER
+                },
+            },
+            {
+                transaction
+            });
+        });
+    },
+    
+    /**
+     * 
+     * @param {QueryInterface} queryInterface 
+     * @param {sequelize} Sequelize 
+     * @returns 
+     */
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.dropTable('strategy_instance_recovery_grid_orders', {transaction});
+            await queryInterface.dropTable('strategy_instance_recovery_grids', {transaction});
+        });
+
+    }
+}
